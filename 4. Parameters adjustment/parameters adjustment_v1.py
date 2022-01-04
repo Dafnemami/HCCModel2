06d8cf31b60203dd7f9bbae75d3e3518b56db72b
DAFNE ARRIAGADA <dafne.arriagada@uc.cl>
2022-01-04 14:25:46 -0300

    P. testear código fx emulador_odeint hasta saber cmo fittear alpha_T

--- 4. Parameters adjustment/parameters adjustment_v1.py
+++ 4. Parameters adjustment/parameters adjustment_v1.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from lmfit import minimize, Parameters, report_fit
 from scipy.integrate import solve_ivp
 from cargar_datos_pacientes import cargar_datos_pacientes
+from ODEs_y_rad_v3_Parameters import emulador_odeint
 import parametros as p
 
 
@@ -19,9 +20,16 @@ import parametros as p
 
 # 2. parametros.py para las constantes
 
+# 3. Trabaja a partir de lo que le retorna emulador_odeint del módulo
+# ODEs_y_rad_v3_Parameters
 
 
 #########################################################
 #                       CÓDIGO                          #
 #########################################################
 
+## TODO 04/01/2022
+## P. patterns of faliure (Pablo Cancer UC)
+    ## Continuare trabajando en este código PARA TESTEAR
+    ## fx emulador_odeint hasta que entienda como ajustar alpha_T
+    ## q sospecho puede ser con el uso de los "patters of faliure"
